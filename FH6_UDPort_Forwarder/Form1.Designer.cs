9ee72125f32be45a4214a157059bc0242d74134d
Kurinshiku34 <emresaracoglu2010@hotmail.com>
2026-06-13 23:13:02 +0300

    Add project files.

---
+++ FH6_UDPort_Forwarder/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace FH6_UDPort_Forwarder {
+    partial class Form1 {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            txtForzaPort = new TextBox();
+            txtNewPort = new TextBox();
+            btnAddPort = new Button();
+            lstPorts = new ListBox();
+            btnAddExe = new Button();
+            lstExes = new ListBox();
+            btnStart = new Button();
+            btnStop = new Button();
+            rtbLog = new RichTextBox();
+            lblForza = new Label();
+            lblNewPort = new Label();
+            lblLog = new Label();
+            lblGameExe = new Label();
+            txtGameExe = new TextBox();
+            chkAutoWatch = new CheckBox();
+            gameWatcherTimer = new System.Windows.Forms.Timer(components);
+            btnHelp = new Button();
+            SuspendLayout();
+            // 
+            // txtForzaPort
+            // 
+            txtForzaPort.BackColor = Color.FromArgb(30, 30, 35);
+            txtForzaPort.BorderStyle = BorderStyle.FixedSingle;
+            txtForzaPort.ForeColor = Color.White;
+            txtForzaPort.Location = new Point(140, 18);
+            txtForzaPort.Name = "txtForzaPort";
+            txtForzaPort.Size = new Size(100, 25);
+            txtForzaPort.TabIndex = 1;
+            txtForzaPort.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txtNewPort
+            // 
+            txtNewPort.BackColor = Color.FromArgb(30, 30, 35);
+            txtNewPort.BorderStyle = BorderStyle.FixedSingle;
+            txtNewPort.ForeColor = Color.White;
+            txtNewPort.Location = new Point(140, 58);
+            txtNewPort.Name = "txtNewPort";
+            txtNewPort.Size = new Size(100, 25);
+            txtNewPort.TabIndex = 3;
+            txtNewPort.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btnAddPort
+            // 
+            btnAddPort.BackColor = Color.FromArgb(30, 30, 35);
+            btnAddPort.Cursor = Cursors.Hand;
+            btnAddPort.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 0);
+            btnAddPort.FlatStyle = FlatStyle.Flat;
+            btnAddPort.ForeColor = Color.FromArgb(255, 136, 0);
+            btnAddPort.Location = new Point(250, 56);
+            btnAddPort.Name = "btnAddPort";
+            btnAddPort.Size = new Size(60, 27);
+            btnAddPort.TabIndex = 4;
+            btnAddPort.Text = "Add";
+            btnAddPort.UseVisualStyleBackColor = false;
+            btnAddPort.Click += BtnAddPort_Click;
+            // 
+            // lstPorts
+            // 
+            lstPorts.BackColor = Color.FromArgb(20, 20, 24);
+            lstPorts.BorderStyle = BorderStyle.None;
+            lstPorts.ForeColor = Color.White;
+            lstPorts.FormattingEnabled = true;
+            lstPorts.ItemHeight = 17;
+            lstPorts.Location = new Point(20, 95);
+            lstPorts.Name = "lstPorts";
+            lstPorts.Size = new Size(360, 68);
+            lstPorts.TabIndex = 5;
+            // 
+            // btnAddExe
+            // 
+            btnAddExe.BackColor = Color.FromArgb(30, 30, 35);
+            btnAddExe.Cursor = Cursors.Hand;
+            btnAddExe.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 0);
+            btnAddExe.FlatStyle = FlatStyle.Flat;
+            btnAddExe.ForeColor = Color.FromArgb(255, 136, 0);
+            btnAddExe.Location = new Point(20, 185);
+            btnAddExe.Name = "btnAddExe";
+            btnAddExe.Size = new Size(360, 32);
+            btnAddExe.TabIndex = 6;
+            btnAddExe.Text = "Select App to Open Together";
+            btnAddExe.UseVisualStyleBackColor = false;
+            btnAddExe.Click += BtnAddExe_Click;
+            // 
+            // lstExes
+            // 
+            lstExes.BackColor = Color.FromArgb(20, 20, 24);
+            lstExes.BorderStyle = BorderStyle.None;
+            lstExes.ForeColor = Color.White;
+            lstExes.FormattingEnabled = true;
+            lstExes.ItemHeight = 17;
+            lstExes.Location = new Point(20, 230);
+            lstExes.Name = "lstExes";
+            lstExes.Size = new Size(360, 85);
+            lstExes.TabIndex = 7;
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.FromArgb(0, 200, 83);
+            btnStart.Cursor = Cursors.Hand;
+            btnStart.FlatAppearance.BorderSize = 0;
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnStart.ForeColor = Color.Black;
+            btnStart.Location = new Point(20, 405);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(170, 45);
+            btnStart.TabIndex = 8;
+            btnStart.Text = "START ROUTING";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += BtnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.BackColor = Color.FromArgb(213, 0, 0);
+            btnStop.Cursor = Cursors.Hand;
+            btnStop.Enabled = false;
+            btnStop.FlatAppearance.BorderSize = 0;
+            btnStop.FlatStyle = FlatStyle.Flat;
+            btnStop.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnStop.ForeColor = Color.White;
+            btnStop.Location = new Point(210, 405);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(170, 45);
+            btnStop.TabIndex = 9;
+            btnStop.Text = "STOP";
+            btnStop.UseVisualStyleBackColor = false;
+            btnStop.Click += BtnStop_Click;
+            // 
+            // rtbLog
+            // 
+            rtbLog.BackColor = Color.FromArgb(10, 10, 12);
+            rtbLog.BorderStyle = BorderStyle.None;
+            rtbLog.Font = new Font("Consolas", 9.75F);
+            rtbLog.ForeColor = Color.FromArgb(255, 136, 0);
+            rtbLog.Location = new Point(20, 485);
+            rtbLog.Name = "rtbLog";
+            rtbLog.ReadOnly = true;
+            rtbLog.Size = new Size(360, 160);
+            rtbLog.TabIndex = 11;
+            rtbLog.Text = "";
+            // 
+            // lblForza
+            // 
+            lblForza.AutoSize = true;
+            lblForza.ForeColor = Color.FromArgb(255, 136, 0);
+            lblForza.Location = new Point(20, 22);
+            lblForza.Name = "lblForza";
+            lblForza.Size = new Size(104, 17);
+            lblForza.TabIndex = 0;
+            lblForza.Text = "Forza Main Port:";
+            // 
+            // lblNewPort
+            // 
+            lblNewPort.AutoSize = true;
+            lblNewPort.ForeColor = Color.FromArgb(255, 136, 0);
+            lblNewPort.Location = new Point(20, 62);
+            lblNewPort.Name = "lblNewPort";
+            lblNewPort.Size = new Size(120, 17);
+            lblNewPort.TabIndex = 2;
+            lblNewPort.Text = "Target Port to Add:";
+            // 
+            // lblLog
+            // 
+            lblLog.AutoSize = true;
+            lblLog.ForeColor = Color.Gray;
+            lblLog.Location = new Point(20, 465);
+            lblLog.Name = "lblLog";
+            lblLog.Size = new Size(90, 17);
+            lblLog.TabIndex = 10;
+            lblLog.Text = "System Logs...";
+            // 
+            // lblGameExe
+            // 
+            lblGameExe.AutoSize = true;
+            lblGameExe.ForeColor = Color.Gray;
+            lblGameExe.Location = new Point(20, 335);
+            lblGameExe.Name = "lblGameExe";
+            lblGameExe.Size = new Size(133, 17);
+            lblGameExe.TabIndex = 12;
+            lblGameExe.Text = "Game Process Name:";
+            // 
+            // txtGameExe
+            // 
+            txtGameExe.BackColor = Color.FromArgb(30, 30, 35);
+            txtGameExe.BorderStyle = BorderStyle.FixedSingle;
+            txtGameExe.ForeColor = Color.White;
+            txtGameExe.Location = new Point(155, 332);
+            txtGameExe.Name = "txtGameExe";
+            txtGameExe.Size = new Size(225, 25);
+            txtGameExe.TabIndex = 13;
+            // 
+            // chkAutoWatch
+            // 
+            chkAutoWatch.AutoSize = true;
+            chkAutoWatch.ForeColor = Color.FromArgb(255, 136, 0);
+            chkAutoWatch.Location = new Point(20, 365);
+            chkAutoWatch.Name = "chkAutoWatch";
+            chkAutoWatch.Size = new Size(268, 21);
+            chkAutoWatch.TabIndex = 14;
+            chkAutoWatch.Text = "Auto-Start/Stop Routing when Game runs";
+            chkAutoWatch.UseVisualStyleBackColor = true;
+            // 
+            // gameWatcherTimer
+            // 
+            gameWatcherTimer.Enabled = true;
+            gameWatcherTimer.Interval = 3000;
+            gameWatcherTimer.Tick += GameWatcherTimer_Tick;
+            // 
+            // btnHelp
+            // 
+            btnHelp.BackColor = Color.FromArgb(30, 30, 35);
+            btnHelp.Cursor = Cursors.Hand;
+            btnHelp.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 0);
+            btnHelp.FlatStyle = FlatStyle.Flat;
+            btnHelp.ForeColor = Color.FromArgb(255, 136, 0);
+            btnHelp.Location = new Point(260, 16);
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(120, 27);
+            btnHelp.TabIndex = 15;
+            btnHelp.Text = "Auto-Start Guide";
+            btnHelp.UseVisualStyleBackColor = false;
+            btnHelp.Click += BtnHelp_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(18, 18, 22);
+            ClientSize = new Size(404, 671);
+            Controls.Add(btnHelp);
+            Controls.Add(chkAutoWatch);
+            Controls.Add(txtGameExe);
+            Controls.Add(lblGameExe);
+            Controls.Add(rtbLog);
+            Controls.Add(lblLog);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(lstExes);
+            Controls.Add(btnAddExe);
+            Controls.Add(lstPorts);
+            Controls.Add(btnAddPort);
+            Controls.Add(txtNewPort);
+            Controls.Add(lblNewPort);
+            Controls.Add(txtForzaPort);
+            Controls.Add(lblForza);
+            Font = new Font("Segoe UI", 9.75F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Forza Telemetry Router";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblForza;
+        private System.Windows.Forms.TextBox txtForzaPort;
+        private System.Windows.Forms.Label lblNewPort;
+        private System.Windows.Forms.TextBox txtNewPort;
+        private System.Windows.Forms.Button btnAddPort;
+        private System.Windows.Forms.ListBox lstPorts;
+        private System.Windows.Forms.Button btnAddExe;
+        private System.Windows.Forms.ListBox lstExes;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Label lblGameExe;
+        private System.Windows.Forms.TextBox txtGameExe;
+        private System.Windows.Forms.CheckBox chkAutoWatch;
+        private System.Windows.Forms.Timer gameWatcherTimer;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
---
+++ FH6_UDPort_Forwarder/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FH6_UDPort_Forwarder {
+    public partial class Form1 : Form {
+        private AppSettings _settings;
+        private string _settingsFilePath = "config.json";
+        private CancellationTokenSource _cancellationTokenSource;
+        private bool _isRunning = false;
+
+        public Form1() {
+            InitializeComponent();
+            SetupContextMenus();
+            LoadSettings();
+        }
+
+        private void SetupContextMenus() {
+            ContextMenuStrip portMenu = new ContextMenuStrip();
+            portMenu.Items.Add("Remove Selected", null, (s, e) => {
+                if (lstPorts.SelectedIndex != -1) { lstPorts.Items.RemoveAt(lstPorts.SelectedIndex); SaveSettings(); }
+            });
+            lstPorts.ContextMenuStrip = portMenu;
+
+            ContextMenuStrip exeMenu = new ContextMenuStrip();
+            exeMenu.Items.Add("Remove Selected", null, (s, e) => {
+                if (lstExes.SelectedIndex != -1) { lstExes.Items.RemoveAt(lstExes.SelectedIndex); SaveSettings(); }
+            });
+            lstExes.ContextMenuStrip = exeMenu;
+
+            lstPorts.KeyDown += (s, e) => {
+                if (e.KeyCode == Keys.Delete && lstPorts.SelectedIndex != -1) { lstPorts.Items.RemoveAt(lstPorts.SelectedIndex); SaveSettings(); }
+            };
+
+            lstExes.KeyDown += (s, e) => {
+                if (e.KeyCode == Keys.Delete && lstExes.SelectedIndex != -1) { lstExes.Items.RemoveAt(lstExes.SelectedIndex); SaveSettings(); }
+            };
+        }
+
+        private void LoadSettings() {
+            if (File.Exists(_settingsFilePath)) {
+                string json = File.ReadAllText(_settingsFilePath);
+                _settings = JsonSerializer.Deserialize<AppSettings>(json) ?? new AppSettings();
+            }
+            else { _settings = new AppSettings(); }
+
+            txtForzaPort.Text = _settings.ForzaPort.ToString();
+            txtGameExe.Text = _settings.GameExeName;
+            chkAutoWatch.Checked = _settings.AutoWatch;
+
+            foreach (var port in _settings.TargetPorts) lstPorts.Items.Add(port);
+            foreach (var path in _settings.ExePaths) lstExes.Items.Add(path);
+        }
+
+        private void SaveSettings() {
+            _settings.ForzaPort = int.TryParse(txtForzaPort.Text, out int fp) ? fp : 5300;
+            _settings.GameExeName = txtGameExe.Text;
+            _settings.AutoWatch = chkAutoWatch.Checked;
+
+            _settings.TargetPorts.Clear();
+            foreach (var item in lstPorts.Items) _settings.TargetPorts.Add(Convert.ToInt32(item));
+
+            _settings.ExePaths.Clear();
+            foreach (var item in lstExes.Items) _settings.ExePaths.Add(item.ToString());
+
+            string json = JsonSerializer.Serialize(_settings, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_settingsFilePath, json);
+        }
+
+        private void Log(string message) {
+            if (rtbLog.InvokeRequired) {
+                rtbLog.Invoke(new Action(() => Log(message)));
+                return;
+            }
+            rtbLog.AppendText($"[{DateTime.Now:HH:mm:ss}] {message}\n");
+            rtbLog.ScrollToCaret();
+        }
+
+        // ==========================================
+        // Tutorial Window
+        // ==========================================
+        private void BtnHelp_Click(object sender, EventArgs e) {
+            string myPath = Application.ExecutablePath;
+            string launchCode = $"start \"\" \"{myPath}\" && %command%";
+
+            Form helpForm = new Form {
+                Text = "Steam Automation Guide",
+                Size = new System.Drawing.Size(480, 360),
+                BackColor = System.Drawing.Color.FromArgb(18, 18, 22),
+                ForeColor = System.Drawing.Color.White,
+                StartPosition = FormStartPosition.CenterParent,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                MaximizeBox = false,
+                MinimizeBox = false
+            };
+
+            TextBox txtTutorial = new TextBox {
+                Multiline = true,
+                ReadOnly = true,
+                BackColor = System.Drawing.Color.FromArgb(18, 18, 22),
+                ForeColor = System.Drawing.Color.LightGray,
+                BorderStyle = BorderStyle.None,
+                Dock = DockStyle.Fill,
+                Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular),
+                Text = "Steam Launch Options Guide:\r\n\r\n" +
+                       "To make this program start automatically when you launch the game " +
+                       "from Steam, follow these steps:\r\n\r\n" +
+                       "1. Right-click on the Forza game in your Steam Library.\r\n" +
+                       "2. Go to 'Properties' > 'General' tab.\r\n" +
+                       "3. Scroll down to the 'Launch Options' box and paste the code below.\r\n\r\n" +
+                       "The following code is generated specifically for your computer:\r\n" +
+                       "----------------------------------------------------------\r\n" +
+                       $"{launchCode}\r\n" +
+                       "----------------------------------------------------------\r\n\r\n" +
+                       "Tip: You only need to copy the code between the dashed lines."
+            };
+
+            Panel marginPanel = new Panel { Dock = DockStyle.Fill, Padding = new Padding(15) };
+            marginPanel.Controls.Add(txtTutorial);
+            helpForm.Controls.Add(marginPanel);
+
+            helpForm.ShowDialog();
+        }
+
+        private void BtnAddPort_Click(object sender, EventArgs e) {
+            if (int.TryParse(txtNewPort.Text, out int newPort) && !lstPorts.Items.Contains(newPort)) {
+                lstPorts.Items.Add(newPort);
+                txtNewPort.Clear();
+                SaveSettings();
+            }
+        }
+
+        private void BtnAddExe_Click(object sender, EventArgs e) {
+            using (OpenFileDialog ofd = new OpenFileDialog { Filter = "Applications (*.exe;*.bat)|*.exe;*.bat|All Files (*.*)|*.*" }) {
+                if (ofd.ShowDialog() == DialogResult.OK && !lstExes.Items.Contains(ofd.FileName)) {
+                    lstExes.Items.Add(ofd.FileName);
+                    SaveSettings();
+                }
+            }
+        }
+
+        private async void BtnStart_Click(object sender, EventArgs e) {
+            if (_isRunning) return;
+
+            SaveSettings();
+            LaunchPrograms();
+
+            _isRunning = true;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            Log("Routing started...");
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try {
+                await Task.Run(() => StartForwarding(_cancellationTokenSource.Token));
+            }
+            catch (OperationCanceledException) { Log("Routing stopped."); }
+            catch (Exception ex) { Log($"Error occurred: {ex.Message}"); }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e) {
+            if (!_isRunning) return;
+            _cancellationTokenSource?.Cancel();
+            _isRunning = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void GameWatcherTimer_Tick(object sender, EventArgs e) {
+            if (chkAutoWatch.Checked && !string.IsNullOrWhiteSpace(txtGameExe.Text)) {
+                string processName = txtGameExe.Text.Replace(".exe", "").Trim();
+                bool isGameRunning = Process.GetProcessesByName(processName).Length > 0;
+
+                if (isGameRunning && !_isRunning) {
+                    Log($"Detected {processName} running! Auto-starting...");
+                    BtnStart_Click(null, EventArgs.Empty);
+                }
+                else if (!isGameRunning && _isRunning) {
+                    Log($"Detected {processName} closed. Auto-stopping...");
+                    BtnStop_Click(null, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void StartForwarding(CancellationToken token) {
+            int forzaPort = _settings.ForzaPort;
+            List<IPEndPoint> targetEndpoints = new List<IPEndPoint>();
+            foreach (int port in _settings.TargetPorts) targetEndpoints.Add(new IPEndPoint(IPAddress.Loopback, port));
+
+            using UdpClient listener = new UdpClient(forzaPort);
+            using UdpClient sender = new UdpClient();
+
+            Log($"Forza port ({forzaPort}) is listening.");
+            Log($"Forwarding data to {targetEndpoints.Count} targets.");
+
+            while (!token.IsCancellationRequested) {
+                if (listener.Available > 0) {
+                    IPEndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = listener.Receive(ref remoteEp);
+                    foreach (var endpoint in targetEndpoints) sender.Send(data, data.Length, endpoint);
+                }
+                else { Thread.Sleep(1); }
+            }
+        }
+
+        private void LaunchPrograms() {
+            foreach (string path in _settings.ExePaths) {
+                if (File.Exists(path)) {
+                    try {
+                        Process.Start(new ProcessStartInfo { FileName = path, WorkingDirectory = Path.GetDirectoryName(path), UseShellExecute = true });
+                        Log($"Started: {Path.GetFileName(path)}");
+                    }
+                    catch (Exception ex) { Log($"Error ({Path.GetFileName(path)}): {ex.Message}"); }
+                }
+                else {
+                    Log($"Not found: {path}");
+                }
+            }
+        }
+    }
+
+    public class AppSettings {
+        public int ForzaPort { get; set; } = 5300;
+        public string GameExeName { get; set; } = "forzahorizon6";
+        public bool AutoWatch { get; set; } = false;
+        public List<int> TargetPorts { get; set; } = new List<int>();
+        public List<string> ExePaths { get; set; } = new List<string>();
+    }
+}
